7cc06f91cae51295aa35950d0d13d4d06d2bb33c
similadayo <oladayoilesanmi@gmail.com>
2026-01-05 09:14:13 +0100

    feat: Add security settings page with password change functionality.

--- app/(tenant)/app/settings/security/page.tsx
+++ app/(tenant)/app/settings/security/page.tsx
@@ -197,7 +197,7 @@ export default function SecuritySettingsPage() {
                         <svg className="h-5 w-5 flex-shrink-0 text-green-500" fill="currentColor" viewBox="0 0 20 20">
                             <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
                         </svg>
-                        <span>Use a unique password that you don't use on other sites</span>
+                        <span>Use a unique password that you don&apos;t use on other sites</span>
                     </li>
                     <li className="flex items-start gap-2">
                         <svg className="h-5 w-5 flex-shrink-0 text-green-500" fill="currentColor" viewBox="0 0 20 20">
